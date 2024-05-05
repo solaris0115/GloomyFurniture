f48b6f8b2c52718c33e1cd225a68b2df394ab122
solaris <solaris0115@naver.com>
2024-05-05 16:44:10 +0900

    remove unnecessary file

--- Symbolic_link.bat
+++
@@ -1,3 +0,0 @@
-rmdir "E:\Program Files (x86)\SteamLibrary\steamapps\common\RimWorld\Mods\GloomyFurniture"
-mklink /d  "E:\Program Files (x86)\SteamLibrary\steamapps\common\RimWorld\Mods\GloomyFurniture" E:\GitProject\GloomyFurniture\GloomyFurniture
-pause
